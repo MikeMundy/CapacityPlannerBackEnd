dd0a82195b18b4801891014e2aeb23a90f84e457
Safavi <ksafavi@corp.westworlds.com>
2022-01-20 17:14:16 -0500

    Added SQL script for creating the DB structure and updated the README file

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CapacityPlannerBackEnd
 Back End for the React Capacity Planner Front End
+
+## Database creation
+
+In order to create the database structure, run the db.sql script located in the root of the project
+
+## Running the API
+
+To run the API, from a command line prompt, run:
+
+`npm run start`
---
+++ db.sql
@@ -0,0 +1,89 @@
+create table Location
+(
+    id   int auto_increment
+        primary key,
+    name varchar(1000) null
+);
+
+create table LocationHoliday
+(
+    id         int auto_increment
+        primary key,
+    locationId int           null,
+    name       varchar(1000) not null,
+    date       datetime      not null,
+    constraint LocationHoliday_Location_id_fk
+        foreign key (locationId) references Location (id)
+            on delete cascade
+);
+
+create table Person
+(
+    id         int auto_increment
+        primary key,
+    locationId int           not null,
+    firstName  varchar(1000) not null,
+    lastName   varchar(1000) not null,
+    constraint Person_Location_id_fk
+        foreign key (locationId) references Location (id)
+            on delete cascade
+);
+
+create table PersonVacation
+(
+    id            int auto_increment
+        primary key,
+    personId      int      not null,
+    date          datetime not null,
+    fractionOfDay float    not null,
+    constraint PersonVacation_Person_id_fk
+        foreign key (personId) references Person (id)
+            on delete cascade
+);
+
+create table ProgramIncrement
+(
+    id   int auto_increment
+        primary key,
+    name varchar(1000) not null
+);
+
+create table Iteration
+(
+    id                 int auto_increment
+        primary key,
+    programIterationId int           not null,
+    name               varchar(1000) not null,
+    startDate          datetime      not null,
+    lengthInDays       int           not null,
+    points             int           not null,
+    constraint Iteration_ProgramIncrement_id_fk
+        foreign key (programIterationId) references ProgramIncrement (id)
+            on delete cascade
+);
+
+create table Team
+(
+    id   int auto_increment
+        primary key,
+    name varchar(1000) not null
+);
+
+create table PersonTeam
+(
+    id         int auto_increment
+        primary key,
+    personId   int           not null,
+    teamId     int           not null,
+    role       varchar(1000) not null,
+    percentage int           not null,
+    constraint PersonTeam_pk
+        unique (personId, teamId),
+    constraint PersonTeam_Person_id_fk
+        foreign key (personId) references Person (id)
+            on delete cascade,
+    constraint PersonTeam_Team_id_fk
+        foreign key (teamId) references Team (id)
+            on delete cascade
+);
+
