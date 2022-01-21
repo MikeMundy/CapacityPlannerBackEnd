a60ee69d411a39847a5846cdec3d074461d60fa3
ksafavi <keikhosro.safavi@outlook.com>
2022-01-20 21:18:53 -0500

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,6 +3,6 @@ module.exports = {
   PORT: process.env.DB_PORT,
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASS,
-  DB: process.env.DB_NAME
+  DB: process.env.DB_NAME,
   DB_SSL: process.env.DB_SSL
 };
