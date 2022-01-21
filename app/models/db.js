f7d0889fa77b09304094cacffab87e3a6982c36f
ksafavi <keikhosro.safavi@outlook.com>
2022-01-20 21:05:29 -0500

    Update db.js

--- app/models/db.js
+++ app/models/db.js
@@ -1,12 +1,19 @@
 const mysql2 = require("mysql2");
 const dbConfig = require("../config/db.config.js");
+const fs = require("fs");
+
+const serverCa = [fs.readFileSync(dbConfig.DB_SSL, "utf8")];
 
 var connection = mysql2.createPool({
   host: dbConfig.HOST,
   port: dbConfig.PORT,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
-  database: dbConfig.DB
+  database: dbConfig.DB,
+  ssl: {
+    rejectUnauthorized: false,
+    ca: serverCa
+  }
 });
 
 module.exports = connection;
